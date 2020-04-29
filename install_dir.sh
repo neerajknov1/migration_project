11a72c8115f878d275a1bf87a0aa835eb9474100
neerajknov1 <neeraj.knov1@gmail.com>
2020-04-29 22:36:31 +0530

    testing

--- install_dir.sh
+++ install_dir.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 echo "Creating Project Directory.."
-mkdir -p oracle/apps/demo
-chmod -R 755 oracle
-cd oracle/apps/demo
-echo "Hi, This is sample Shell Script project" >> ReadMe.txt
-echo "It will create a custom direcory and a readme file" >> ReadMe.txt
-echo "Thank You!!"
+unzip -o oracle.zip
 
